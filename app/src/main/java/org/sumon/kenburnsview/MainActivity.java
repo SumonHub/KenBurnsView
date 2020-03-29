279f90818e888e545c997058fc0d3310c06ad7ce
SumOn <sumon.v0.0@gmail.com>
2020-03-30 00:16:25 +0600

    Initial commit

---
+++ app/src/main/java/org/sumon/kenburnsview/MainActivity.java
@@ -0,0 +1,70 @@
+package org.sumon.kenburnsview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.sumon.library.KenBurnsView;
+import org.sumon.library.LoopViewPager;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        _initializeKenBurnsView();
+    }
+
+    private void _initializeKenBurnsView() {
+        // KenBurnsView
+        final KenBurnsView kenBurnsView = (KenBurnsView) findViewById(R.id.ken_burns_view);
+        kenBurnsView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        kenBurnsView.setSwapMs(3750);
+        kenBurnsView.setFadeInOutMs(750);
+
+        // ResourceIDs
+        List<Integer> resourceIDs = Arrays.asList(SampleImages.IMAGES_RESOURCE);
+        kenBurnsView.loadResourceIDs(resourceIDs);
+
+        // LoopViewListener
+        LoopViewPager.LoopViewPagerListener listener = new LoopViewPager.LoopViewPagerListener() {
+            @Override
+            public View OnInstantiateItem(int page) {
+                TextView counterText = new TextView(getApplicationContext());
+                counterText.setText(String.valueOf(page));
+                return counterText;
+            }
+
+            @Override
+            public void onPageScroll(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                kenBurnsView.forceSelected(position);
+            }
+
+            @Override
+            public void onPageScrollChanged(int page) {
+            }
+        };
+
+        // LoopView
+        LoopViewPager loopViewPager = new LoopViewPager(this, resourceIDs.size(), listener);
+
+        FrameLayout viewPagerFrame = findViewById(R.id.view_pager_frame);
+        viewPagerFrame.addView(loopViewPager);
+
+        kenBurnsView.setPager(loopViewPager);
+    }
+}
---
+++ app/src/main/java/org/sumon/kenburnsview/SampleImages.java
@@ -0,0 +1,12 @@
+package org.sumon.kenburnsview;
+
+public final class SampleImages {
+
+    public static final Integer[] IMAGES_RESOURCE = new Integer[]{
+            R.drawable.sample1,
+            R.drawable.sample2,
+            R.drawable.sample3,
+            R.drawable.sample4,
+            R.drawable.sample5
+    };
+}
